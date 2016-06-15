bc98cb32dc59625b8494144b0ff6b169fd22d1fc
knaseem <knaseem22@gmail.com>
2016-06-15 13:18:14 -0400

    Added Pan Gesture feature SecondViewController

--- GestureRecognizerDemo/SecondViewController.swift
+++ GestureRecognizerDemo/SecondViewController.swift
@@ -11,6 +11,15 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    
+    @IBAction func handlePan(recognizer: UIPanGestureRecognizer) {
+        let translation = recognizer.translationInView(self.view)
+        if let view = recognizer.view {
+            view.center = CGPoint(x: view.center.x + translation.x, y: view.center.y + translation.y)
+        }
+        
+        recognizer.setTranslation(CGPointZero, inView: self.view)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
